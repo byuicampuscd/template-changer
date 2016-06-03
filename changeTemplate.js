73351d9951be82e84e9fc9426bf80aa92473029b
andrewgremlich <andrew.gremlich@gmail.com>
2016-06-02 18:10:11 -0600

    Cheerio .html bug fix.

--- changeTemplate.js
+++ changeTemplate.js
@@ -2,66 +2,68 @@
 "use strict";
 
 var htmlParse = require('cheerio'),
-   prettyHtml = require('js-beautify').html,
-   enums = require('./enums.js'),
+    prettyHtml = require('js-beautify').html,
+    enums = require('./enums.js'),
     path = require('path');
 
 function processTemplate(name, text, options, templates) {
 
-   var $ = htmlParse.load(text),
-      objOut = {
-         name: name,
-         textIn: text,
-         textOut: "",
-         templateUsed: ""
-      },
-      hasMatchInFile;
+    var $ = htmlParse.load(text),
+        objOut = {
+            name: name,
+            textIn: text,
+            textOut: "",
+            templateUsed: ""
+        },
+        hasMatchInFile;
 
-   function parseTitlename(title) {
-      var tiParsed = path.parse(title);
-      return tiParsed.name;
-   }
+    function parseTitlename(title) {
+        var tiParsed = path.parse(title);
+        return tiParsed.name;
+    }
 
-   function fixTitleTag(templateTopIn){
-      var templateTop = htmlParse.load(templateTopIn);
-      templateTop('title').first().text(parseTitlename(name));
-      return templateTop.html();
-   }
+    function fixTitleTag(templateTopIn) {
+        var templateTop = htmlParse.load(templateTopIn);
+        templateTop('title').first().text(parseTitlename(name));
+//        console.log(templateTop.html().split(/(<\/div>){2}/g)[0]);
+        return templateTop.html().split(/(<\/div>){2}/g)[0];
+    }
 
-   function run(template) {
-      objOut.textOut = prettyHtml(fixTitleTag(template.top) + '\n' + $(options.selector).html() + '\n' + template.bottom);
-      objOut.templateUsed = template.name;
-   }
+    function run(template) {
+//        console.log(fixTitleTag(template.top) + "FIRST PART", $(options.selector).html() + "SEC PART", template.bottom + "THIRD PART");
+        objOut.textOut = prettyHtml(fixTitleTag(template.top) + '\n' + $(options.selector).html() + '\n' + template.bottom);
+        objOut.templateUsed = template.name;
+    }
 
-   //if its just normal
-   if (options.mode === enums.mode.NORMAL) {
-      run(templates[0]);
-      return objOut;
-   }
+    //if its just normal
+    if (options.mode === enums.mode.NORMAL) {
+        run(templates[0]);
+        return objOut;
+    }
 
-   //if we need to check first
-   if (options.mode === enums.mode.IF || options.mode === enums.mode.IF_ELSE) {
-      //check if we have a match in the current file
-      hasMatchInFile = $(options.selectorIf).length > 0;
+    //if we need to check first
+    if (options.mode === enums.mode.IF || options.mode === enums.mode.IF_ELSE) {
+        //check if we have a match in the current file
+        hasMatchInFile = $(options.selectorIf).length > 0;
 
-      //run template in IF or IF_ELSE if have match
-      if (hasMatchInFile) {
-         run(templates[0]);
-      }
+        //run template in IF or IF_ELSE if have match
+        if (hasMatchInFile) {
+            run(templates[0]);
+        }
 
-      //if no match but in IF_ELSE
-      if (!hasMatchInFile && options.mode === enums.mode.IF_ELSE) {
-         run(templates[1]);
-      }
-      return objOut;
-   }
+        //if no match but in IF_ELSE
+        if (!hasMatchInFile && options.mode === enums.mode.IF_ELSE) {
+            run(templates[1]);
+        }
+        return objOut;
+    }
 
-   //if type doesn't match
-   throw "Invalid mode type";
+    //if type doesn't match
+    throw "Invalid mode type";
 }
 
 module.exports = function (textInList, options, templates) {
-   return textInList.map(function (text) {
-      return processTemplate(text.name, text.textIn, options, templates);
-   });
+    return textInList.map(function (text) {
+        return processTemplate(text.name, text.textIn, options, templates);
+    });
 };
